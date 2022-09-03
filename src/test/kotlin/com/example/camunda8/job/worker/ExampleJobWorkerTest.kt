79470af9c04105d7af9785331c7518dca94c781e
popov <aptem336@gmail.com>
2022-09-03 20:52:21 +0800

    test user task handling

--- src/test/kotlin/com/example/camunda8/job/worker/ExampleJobWorkerTest.kt
+++ src/test/kotlin/com/example/camunda8/job/worker/ExampleJobWorkerTest.kt
@@ -1,9 +1,10 @@
 package com.example.camunda8.job.worker
 
 import io.camunda.zeebe.client.ZeebeClient
-import io.camunda.zeebe.process.test.assertions.BpmnAssert
 import io.camunda.zeebe.process.test.assertions.BpmnAssert.*
+import io.camunda.zeebe.protocol.Protocol.USER_TASK_JOB_TYPE
 import io.camunda.zeebe.spring.test.ZeebeSpringTest
+import io.camunda.zeebe.spring.test.ZeebeTestThreadSupport.waitForProcessInstanceCompleted
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -16,13 +17,30 @@ class ExampleJobWorkerTest {
 
     @Test
     fun exampleJobWorkerExecuteTest() {
-        val processInstanceResult = zeebeClient
+        val processInstanceEvent = zeebeClient
             ?.newCreateInstanceCommand()
             ?.bpmnProcessId("camunda-8-example-process")
             ?.latestVersion()
-            ?.withResult()
             ?.send()
             ?.join()
-        assertThat(processInstanceResult)
+        assertThat(processInstanceEvent).isStarted
+
+        assertThat(processInstanceEvent).isWaitingAtElements(
+            "Activity_ServiceTask",
+            "Activity_UserTask"
+        )
+
+        val jobs = zeebeClient
+            ?.newActivateJobsCommand()
+            ?.jobType(USER_TASK_JOB_TYPE)
+            ?.maxJobsToActivate(1)
+            ?.send()
+            ?.join()
+            ?.jobs
+
+        jobs?.forEach { activatedJob -> zeebeClient?.newCompleteCommand(activatedJob.key)?.send()?.join() }
+
+        waitForProcessInstanceCompleted(processInstanceEvent)
+        assertThat(processInstanceEvent).isCompleted
     }
 }
